2d65538e933037a8d38619e784e085eb984e814b
Eugene <jdorovko@gmail.com>
2022-08-26 11:58:52 +0300

    Task-10 restored NavBar

--- src/modules/NavBar/NavBar.tsx
+++ src/modules/NavBar/NavBar.tsx
@@ -1,4 +1,24 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+import {Link} from "react-router-dom";
+
 const NavBar = () => {
-  return <p>NavBar</p>;
-};
+  return (
+      <div className={styles.main}>
+        <Link to={"/main"}>
+          <p>Каталог</p>
+        </Link>
+        <Link to={"/statistics"}>
+          <p>Статистика</p>
+        </Link>
+        <Link to={"/feedBack"}>
+          <p>Обратная связь</p>
+        </Link>
+        <div className={styles.box} />
+        <Link to={"/profile"}>
+          <p>Профиль</p>
+        </Link>
+      </div>
+  )
+}
 export default NavBar;
