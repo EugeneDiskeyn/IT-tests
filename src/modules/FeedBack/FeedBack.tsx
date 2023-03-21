69e519b924e2b8c1661ae496afdb9f96b711771a
Eugene <jdorovko@gmail.com>
2023-03-21 11:35:11 +0300

    Put img and form on feedback page to their positions

--- src/modules/FeedBack/FeedBack.tsx
+++ src/modules/FeedBack/FeedBack.tsx
@@ -2,39 +2,43 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Input from "../../components/Input/Input";
 import styles from "./FeedBack.module.css";
+import assistant from "../../images/logos/virtual-assistant.svg";
 
 const FeedBack = () => {
   return (
     <div>
       <NavBar />
-      <form className={styles.feedback}>
-        <h1>Мы на связи</h1>
-        <Input
-          placeholder={"Логин"}
-          type={"text"}
-          isDisabled={false}
-          isRequired={true}
-        />
-        <Input
-          placeholder={"E-mail"}
-          type={"text"}
-          isDisabled={false}
-          isRequired={true}
-        />
-        <Input
-          placeholder={"Телефон"}
-          type={"text"}
-          isDisabled={false}
-          isRequired={true}
-        />
-        <select className={styles.sel}>
-          <option>Option1</option>
-          <option>Option2</option>
-          <option>Option3</option>
-          <option>Option4</option>
-        </select>
-        <textarea className={styles.feedarea} />
-      </form>
+      <div className={styles.form_img}>
+        <form className={styles.feedback}>
+          <h1>Мы на связи</h1>
+          <Input
+            placeholder={"Логин"}
+            type={"text"}
+            isDisabled={false}
+            isRequired={true}
+          />
+          <Input
+            placeholder={"E-mail"}
+            type={"text"}
+            isDisabled={false}
+            isRequired={true}
+          />
+          <Input
+            placeholder={"Телефон"}
+            type={"text"}
+            isDisabled={false}
+            isRequired={true}
+          />
+          <select className={styles.sel}>
+            <option>Option1</option>
+            <option>Option2</option>
+            <option>Option3</option>
+            <option>Option4</option>
+          </select>
+          <textarea className={styles.feedarea} />
+        </form>
+        <img src={assistant} alt={"virtual assistant"} />
+      </div>
     </div>
   );
 };
